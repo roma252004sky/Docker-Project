3b8b212be0d8d794420d25eddb8a2a39658b9604
Roman Andreev <103677616+roma252004sky@users.noreply.github.com>
2024-06-10 20:48:55 +0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 echo "Building backend..."
-docker build -t project/backend:latest ./backend
+docker build -t project/backend:latest ./java
 if [ $? -ne 0 ]; then
   echo "Backend build failed"
   exit 1
 fi
 
 echo "Building frontend..."
-docker build -t project/frontend:latest ./frontend
+docker build -t project/frontend:latest ./react
 if [ $? -ne 0 ]; then
   echo "Frontend build failed"
   exit 1
